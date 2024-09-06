94c83352fa4922f8bce0b62b535e665152a39e3e
Valentina Toni <vale.toni93@gmail.com>
2024-09-06 15:47:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./ui/button"
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click Me!</Button>
+    </>
+  )
+}
+
+export default App
