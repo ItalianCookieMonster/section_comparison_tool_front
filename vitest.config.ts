c58e608bc26d3504c7b9bea0d83b0278e3956404
Valentina Toni <vale.toni93@gmail.com>
2024-09-29 16:39:24 +0200

    DONE: tried to test

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     plugins: [react()],
     test: {
+        globals: true,
         environment: 'jsdom',
+        setupFiles: ['./tests/setupTests.ts'],
+        include: ['tests/**/*.test.{js,ts,jsx,tsx}'],
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+            exclude: ['tests', 'node_modules']
+        },
     },
-    
+
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
